3e02a3a7230ec34d419a001a1b1b4450ecb2d299
Dan Green <danngreen1@gmail.com>
2021-11-20 13:23:00 -0800

    Fixed pcm script

--- PCM/make_archive.sh
+++ PCM/make_archive.sh
@@ -10,12 +10,12 @@ echo "Copying metadata.json and icon.png"
 cp metadata.json PCM/metadata.json
 cp resources/icon.png PCM/icon.png
 
-shasum --algorithm 256 PCM/4ms-kicad-lib-$version.zip
-ls -l PCM/4ms-kicad-lib-$version.zip
-unzip -l PCM/4ms-kicad-lib-$version.zip
+download_sha256=$(shasum --algorithm 256 PCM/4ms-kicad-lib-$version.zip | xargs | cut -d' ' -f1)
+download_size=$(ls -l PCM/4ms-kicad-lib-$version.zip | xargs | cut -d' ' -f5)
+install_size=$(unzip -l PCM/4ms-kicad-lib-$version.zip | tail -1 | xargs | cut -d' ' -f1)
 
 echo "Edit these json values in PCM/metadata.json:"
-echo "\"download_sha256\": \"(sha)\","
-echo "\"download_size\": (zipsize),"
-echo "\"download_url\": \"https://github.com/4ms/4ms-kicad-lib/archive/4ms-kicak-lib-$version.zip\","
-echo "\"install_size\": (unzippedsize),"
+echo "\"download_sha256\": \"$download_sha256\","
+echo "\"download_size\": $download_size,"
+echo "\"download_url\": \"https://github.com/4ms/4ms-kicad-lib/archive/4ms-kicad-lib-$version.zip\","
+echo "\"install_size\": $install_size,"
